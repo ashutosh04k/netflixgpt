bc0428f5f83d4d661e947f1a78e158667175c6f5
ashutosh04k <kumarasutosh2014@gmail.com>
2024-06-07 22:49:44 +0530

    main

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="text-3xl font-bold text-green-800">
-      lets begin the show
+      lets begin the show and work on main directory
       
+
     </div>
   );
 }
