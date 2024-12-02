e62fade8568ba50538e2c8d883073d3b2c7cd914
ashutosh04k <kumarasutosh2014@gmail.com>
2024-12-02 22:00:58 +0530

    bug fix of overflow

--- src/Component/VideoBackground.js
+++ src/Component/VideoBackground.js
@@ -8,9 +8,9 @@ const VideoBackground = ({ movieId }) => {
 
   return (
 
-    <div className="w-screen">
+    <div className="w-full h-screen">
       <iframe
-        className="w-screen aspect-video"
+        className="w-full aspect-video"
         src={
           `https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&loop=1&playlist=${trailerVideo?.key}`
         }
